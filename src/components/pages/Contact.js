7cc65482786fb5768930a58ad160192635e78ae6
Jordan Bradley <jordanrhysbradley@gmail.com>
2021-09-29 12:48:51 +1000

    a bit of extra CSS and boilerplate HTML

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
 const Home = () => {
-    return (
+    return (<>
         <div className="page-heading">
             <h1>
             Contact
             </h1>
-            <p>name</p>
         </div>
+        <div className="contact-form">
+            <form>
+             <div id="enquiry-table" className="container-fluid">
+                <label className="contact-title">First Name:</label>
+                <input type="text" name="first_name" className="contact-filling" />
+                <label className="contact-title">Last Name:</label>
+                <input type="text" name="last_name" className="contact-filling" />
+                <label className="contact-title">Organisation:</label>
+                <input type="text" name="organisation" className="contact-filling" />
+                <label className="contact-title">Email:</label>
+                <input type="email" name="email" className="contact-filling" />
+                <label className="contact-title">Phone:</label>
+                <input type="tel" name="phone_number" className="contact-filling" />
+                <label id="enquiry-content" className="contact-title">Enquiry:</label>
+                <input type="text" name="description" className="contact-filling6"/>
+                <button id="button">Submit</button>
+             </div>
+            </form>
+        </div>
+        </>
     )
 }
 
